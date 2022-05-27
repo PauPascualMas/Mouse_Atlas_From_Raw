b783fa83003a4dfb455cd5b6aee1c9c259561ac5
Pau Pascual Mas <93324291+PauPascualMas@users.noreply.github.com>
2022-05-27 17:11:44 +0200

    Update download_genome_and_annot.sh

--- download_genome_and_annot.sh
+++ download_genome_and_annot.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 #SBATCH --partition=normal 
-#SBATCH --job-name=move 
+#SBATCH --job-name=dwonload
 #SBATCH --nodes=1 
-#SBATCH -o /homes/users/ppascual/scratch/STAR_Align/Scripts/Output_info/slurm.%j.out
-#SBATCH -e /homes/users/ppascual/scratch/STAR_Align/Scripts/Output_info/slurm.%j.err
+
 
 module load wget/1.20.3-GCCcore-9.3.0 
 
 # download optimized Genome and Annotation https://www.thepoollab.org/resources
 
-DATA_DIR=/homes/users/ppascual/scratch/STAR_Align/Data/
+mkdir /path/to/genome
+mkdir /path/to/annotation
 
+GEN_DIR=/path/to/genome
+ANNOT_DIR=/path/to/annot
 
-wget -P $DATA_DIR/Genome https://storage.googleapis.com/generecovery/mouse_mm10_optimized_v1.tar.gz
+wget -P $GEN_DIR/ https://storage.googleapis.com/generecovery/mouse_mm10_optimized_v1.tar.gz
 
-wget -P $DATA_DIR/Annotation https://storage.googleapis.com/generecovery/mouse_mm10_optimized_v1.gtf.gz
+wget -P $ANNOT_DIR/ https://storage.googleapis.com/generecovery/mouse_mm10_optimized_v1.gtf.gz
