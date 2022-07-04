29bb26f7c5d1fca1c89b051e95f1f5ce6eb95421
Pau Pascual Mas <93324291+PauPascualMas@users.noreply.github.com>
2022-07-04 16:19:17 +0200

    Update alignment.sh

--- alignment.sh
+++ alignment.sh
@@ -9,10 +9,9 @@
 #SBATCH -e /homes/users/ppascual/scratch/STAR_Align/Scripts/Output_info/slurm.%j.err
 
 
-## ALIGNMENT AND CREATION OF RAW COUNTS MATRIX ##
-#---------   STAR 2.7.10a (STARsolo)----------
+## ALIGNMENT: STARsolo (STAR 2.7.10a) ##
 
-# STARsolo takes cDNA (R1) read and CB+UMI (R2+R3) read to perform the alignment. 
+# STARsolo takes cDNA (R1) read and CB+UMI (R2+R3, which needs to be created) read to perform the alignment. 
 # Genome (.fa) and annotation file (.gtf) are also provided. 
 # Barcode whitelist (.txt) can be provided as guide of which BC are real.
 
@@ -20,40 +19,36 @@ interactive
 module load  STAR/2.7.10a-GCC-11.2.0
 module load gzip/1.10-GCCcore-11.2.0 
 
-ALIGN_DATA='/homes/users/ppascual/scratch/STAR_Align/Data'
+ALIGN_DATA='/path/to/Data'
 GENOME_FILE="${ALIGN_DATA}/Genome/genome.fa"
 ANNOTATION_FILE="${ALIGN_DATA}/Annotation/mouse_mm10_optimized_v1.gtf.gz"
-ALIGN_RESULTS='/homes/users/ppascual/scratch/STAR_Align/Results/Alignment_Pijuan'
+ALIGN_RESULTS='path/to/Results/Alignment_Pijuan'
 READS_DIR="${ALIGN_DATA}/Reads/Pijuan"
 UMI_whitelist="${ALIGN_DATA}/BC_whitelist/BC_whitelist_v1/737K-april-2014_rc.txt"
 
 ## Genome Index generation is preliminary in the alignment by STAR
-## In our case, optimized genome and annotation used, as well as index are directly obtained from https://www.thepoollab.org/resources
+## In our case, index is directly obtained from https://www.thepoollab.org/resources, as well as the optimized genome and reference annotation
 
 ## If willing to generate your own index, use the following command:
 : '
 STAR \
 --runMode genomeGenerate \
 --runThreadN 8 \
---genomeDir /homes/users/ppascual/scratch/STAR_Align/Data/Genome/ \
+--genomeDir $ALIGN_DATA/Genome/ \
 --genomeFastaFiles $GENOME_FILE \
 --sjdbGTFfile $ANNOTATION_FILE \
 --genomeSAindexNbases 12 \
 --limitGenomeGenerateRAM 10000000000'
 
-
-### STARsolo
-
-#Select stage and sample to align
+#Select stage and sample to align (change it manually for every sample)
 
 E_stage='E_8.5' 
 Sample='Sample_37' 
 
 cd $READS_DIR/${E_stage}/${Sample}
 
-#creating fastq file merging cb and umi (in 10x Chemistry v1 cb and umi are in separate files), as required for the alignment
+#creating fastq file merging CB and UMI (in 10x Chemistry v1 are in separate files), as required for the alignment
 
-rm -rf CB_UMI_merged.fastq.gz #remove preexisting file, just in case
 paste <(zcat merged_R2.fastq.gz) <(zcat merged_R3.fastq.gz) | awk '{if (NR%4==1) {print $1 " " $2} else if (NR%4==3) {print "+"} else {print $1 $2}}' > CB_UMI_merged.fastq
 gzip CB_UMI_merged.fastq
 chmod u+x CB_UMI_merged.fastq.gz
@@ -65,6 +60,10 @@ fw='merged_R1_trimmed.fq.gz'
 cb_umi='CB_UMI_merged.fastq.gz'
 
 cd 
+
+### Running STAR
+# Parameters are set to resemble CellRanger results, as well as matching CB and UMI length for v1 Chemistry
+# For other protocols, specifications are attached below with the required vell barcode whitelist too.
 
 STAR \
 --genomeDir $ALIGN_DATA/Genome \
